c4fb37b1221025e0a3b597433e2529b1a826cb5a
Md. Ammar Shahab <ashahab007@gmail.com>
2026-04-15 16:00:06 +0600

    download cv button works but need to change the layout

--- components/web/Navbar.jsx
+++ components/web/Navbar.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 import Image from "next/image";
 import ThemeToggle from "./ThemeToggle/ThemeToggle";
+import { Button } from "../ui/button";
 
 // Navigation links config — easy to extend
 const NAV_LINKS = [
@@ -105,8 +106,24 @@ export default function Navbar() {
               shadow-[0_4px_16px_rgba(99,14,212,0.3)] hover:shadow-[0_6px_20px_rgba(99,14,212,0.4)]
               transition-shadow duration-300"
           >
-            Download Resume
+            <Button
+              className="hidden xl:block bg-linear-to-br from-violet-600 to-purple-700
+              text-white text-sm font-bold px-5 py-2.5 rounded-full
+              shadow-[0_4px_16px_rgba(99,14,212,0.3)] hover:shadow-[0_6px_20px_rgba(99,14,212,0.4)]
+              transition-shadow duration-300"
+              asChild
+            >
+              <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                Download Resume
+              </a>
+            </Button>
           </motion.button>
+
+          {/* <Button asChild>
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              Download Resume
+            </a>
+          </Button> */}
 
           {/* Hamburger — mobile only */}
           <button
